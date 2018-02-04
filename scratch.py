6cfdcf63f09927a835d570dfa8818fa5971c3b80
Errol Flynn <ekusiolek@icloud.com>
2018-02-03 19:33:39 -0600

    list comprehensions

--- scratch.py
+++ scratch.py
@@ -1,1 +1,11 @@
 
+nums = [1,2,3]
+name = 'butthead'
+
+listcomp = [x*10 for x in nums]
+listcompname = [c.upper() for c in name]
+listtype = [str(n) for n in nums]
+
+print(listcomp)
+print(listcompname)
+print(listtype)
