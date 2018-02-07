f3161248abcd6667f808d04adcf012259dfc5de6
Errol Flynn <ekusiolek@icloud.com>
2018-02-06 21:24:18 -0600

    uh, not sure

--- dicts2.py
+++ dicts2.py
@@ -42,6 +42,48 @@ print(stock_list)
 # remove 'cake' from stock_list USE A DICTIONARY METHOD
 stock_list.pop('cake')
 print(stock_list)
-
+print()
 #Dict comprehension
 
+numbers = dict(first = 1, second = 2, third = 3)
+
+sq_numbers = {key: val**2  for key,val in numbers.items()}
+print (sq_numbers)
+
+calling = dict(first='hi', second='there', third='you', fourth='loser')
+
+yelling = {num: word.upper()  for num, word in calling.items()}
+
+sentence = ' '.join(yelling.values())
+print(sentence)
+
+sentence2 = f"{yelling['first']} {yelling['second']}, {yelling['third']} {yelling['fourth']}!"
+print(sentence2)
+
+num_list = [1,2,3,4]
+
+nume_o = {i: 'even' if i%2 == 0 else 'odd'  for i in num_list}
+print(nume_o)
+numek = {i: 'even'  for i in num_list if i %2==0 }
+print(numek)
+
+l = ["CA", "NJ", "RI"]
+l2 = ["California", "New Jersey", "Rhode Island"]
+
+# make sure your solution is assigned to the answer variable so the tests can work!
+answer = {l[i]: l2[i]  for i in range(0, len(l))}
+print(answer)
+
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+
+# use the person variable in your answer
+answer = {k: v  for k,v in person}
+print(answer)
+
+# make sure your solution is assigned to the answer variable so the tests can work!
+answer = dict.fromkeys('aeiou', 0)
+print(answer)
+
+# make sure your solution is assigned to the answer variable so the tests can work!
+answer = {i: chr(i)  for i in range(65, 91)}
+print(answer)
