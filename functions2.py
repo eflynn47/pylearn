7987c73a5549f8ce01d2a16bb067252032865ef8
Errol Flynn <ekusiolek@icloud.com>
2018-02-09 18:03:29 -0600

    added functions3.py (**args)

--- functions2.py
+++ functions2.py
@@ -16,6 +16,36 @@ def multiply_even_numbers(li):
 def capitalize(s):
     return s[0].upper() + s[1:]
 
+# def compact(li):
+#     new_li = []
+#     for val in li:
+#         if val:
+#             new_li.append(val)
+#     return new_li
+
+def compact(li):
+    return [val for val in li if val]
+
+def intersection(li1, li2):
+    return [ val for val in set(li1).intersection(set(li2))]
+
+def isEven(num):
+    return num % 2 == 0
+
+# def partition(li1, func):
+#     tlist = []
+#     flist = []
+#     for val in li1:
+#         if func(val):
+#             tlist.append(val)
+#         else:
+#             flist.append(val)
+#     return [tlist, flist]
+
+def partition(li1, func):
+    return [[val for val in li1 if func(val)], [val for val in li1 if not func(val)]]
+
+
 if __name__ == "__main__":
     print(is_palindrome('testing')) # False
     print(is_palindrome('tacocat')) # True
@@ -31,3 +61,10 @@ if __name__ == "__main__":
 
     print(capitalize("tim")) # "Tim"
     print(capitalize("matt")) # "Matt"
+
+    print(compact([0,1,2,"",[], False, {}, None, "All done"])) # [1,2, "All done"]
+
+    print(intersection([1,2,3], [2,3,4]))    #[2,3]
+    print(intersection(['a','b','z'], ['x','y','z']))  # ['z']
+
+    print(partition([1,2,3,4], isEven)) # [[2,4],[1,3]]
